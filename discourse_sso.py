4ffc5b9bf5e4f3bd4adee7edd0f45fb913c85fda
Justin Welenofsky <justin@welenofsky.com>
2014-09-23 10:53:29 -0700

    moved discourse_sso.py to discoursesso.py

--- discourse_sso.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python3
-
-class Discourse_sso:
-    def __init__(self, secret):
-        self.__secret = secret
-        print(self.__secret) 
-
-    def validate(payload, sig):
-	    pass
